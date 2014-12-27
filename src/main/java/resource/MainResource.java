9e74854558e83865b3b95327000d3b9eb1124ed0
ed0906 <ed.wilson2@hotmail.co.uk>
2014-12-27 14:32:37 +0000

    Moved Webservice Skeleton project into its own repository

---
+++ src/main/java/resource/MainResource.java
@@ -0,0 +1,30 @@
+package resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("/api")
+public class MainResource {
+
+	@GET
+	@Path("/healthcheck")
+	public Response healthCheck() {
+		return Response.ok().build();
+	}
+
+	@GET
+	@Path("/talk/{var}")
+	public Response command(@PathParam("var") String msg) {
+		return Response.ok(msg).build();
+	}
+	
+	@GET
+	@Path("/talk")
+	public Response commandQuery(@QueryParam("msg") String msg){
+		return Response.ok(msg).build();
+	}
+
+}
---
+++ src/main/java/service/WebserviceRunner.java
@@ -0,0 +1,58 @@
+package service;
+
+import java.io.File;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class WebserviceRunner {
+
+	private Server server;
+	private int port;
+	private String host;
+
+	public WebserviceRunner(String host, int port) {
+		this.server = new Server(port);
+		this.host = host;
+		this.port = port;
+	}
+
+	public String getWebXml() {
+		return "WebRoot/WEB-INF/web.xml";
+	}
+
+	public String getResourceBase() {
+		return "./";
+	}
+
+	public void start() throws Exception {
+		HandlerCollection handlers = new HandlerCollection();
+		WebAppContext handler = new WebAppContext();
+		handler.setResourceBase(getResourceBase());
+
+		final String webXmlAddress = getWebXml();
+		final File webXml = new File(webXmlAddress);
+		if (!webXml.exists()) {
+			throw new RuntimeException("web.xml not found at: " + webXml.getAbsolutePath());
+		}
+
+		handler.setDescriptor(webXmlAddress);
+		handler.setContextPath("/");
+		handler.setClassLoader(Thread.currentThread().getContextClassLoader());
+
+		handlers.addHandler(handler);
+
+		server.setHandler(handlers);
+		server.start();
+		System.out.println("API started... at 'http://" + getHostAndPort() + "'");
+	}
+	
+	private String getHostAndPort() {
+		return host + ":" + port;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new WebserviceRunner("localhost", 8315).start();
+	}
+}
