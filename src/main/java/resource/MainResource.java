2a423fc2148c00ec54b2b6d1b8adf3f2908148ac
Edward Wilson <ed.wilson2@hotmail.co.uk>
2015-04-26 18:21:37 +0100

    Added basic logging + IntelliJ gitignore files

--- src/main/java/resource/MainResource.java
+++ src/main/java/resource/MainResource.java
@@ -6,24 +6,31 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/rest")
 public class MainResource {
+	private Logger log = LoggerFactory.getLogger(MainResource.class);
 
 	@GET
 	@Path("/healthcheck")
 	public Response healthCheck() {
+		log.debug("Incoming Request: Healthcheck");
 		return Response.ok().build();
 	}
 
 	@GET
 	@Path("/talk/{var}")
 	public Response command(@PathParam("var") String msg) {
+		log.debug("Incoming Request: Talk");
 		return Response.ok(msg).build();
 	}
 	
 	@GET
 	@Path("/talk")
 	public Response commandQuery(@QueryParam("msg") String msg){
+		log.debug("Incoming Request: Talk");
 		return Response.ok(msg).build();
 	}
 
